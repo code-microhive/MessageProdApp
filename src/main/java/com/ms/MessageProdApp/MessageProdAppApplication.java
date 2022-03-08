0160aa382689f32df9ad3b14c0426a32a7f42cfd
tahirghori <developer.tahirghori@gmail.com>
2022-03-08 10:43:40 +0500

    init repo

---
+++ src/main/java/com/ms/MessageProdApp/MessageProdAppApplication.java
@@ -0,0 +1,13 @@
+package com.ms.MessageProdApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageProdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageProdAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/MessageProdApp/MessageProdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.MessageProdApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageProdAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
